3e89f82e48a7e9d54050c6782bf3ee5dde72f7c3
joseph <joebeck03@icloud.com>
2023-04-14 18:58:41 +0100

    minor refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/index.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,18 @@
-export default function Projects() {
-    return (
-        <main>
+import styles from '@/styles/projects.module.css'
+import Link from 'next/link';
 
-        </main>
-    );
+export default function Projects() {
+  return (
+    <main className={styles.main}>
+
+      <div className={``}>
+
+      </div>
+
+      <div className={`${styles.navBar}`}>
+        <Link href="#busb-bot-section">BUSB Bot</Link>
+        <Link href="#">...</Link>
+      </div>
+    </main>
+  );
 }
