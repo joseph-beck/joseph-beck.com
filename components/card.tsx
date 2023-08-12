cb5460f3a2442a36be04c879aae04a4d301575d6
joseph-beck <joebeck03@icloud.com>
2023-08-12 14:59:56 +0100

    start components

---
+++ components/card.tsx
@@ -0,0 +1,45 @@
+interface CardProps {
+  img?: string;
+  alt?: string;
+  title?: string;
+  body?: string;
+};
+
+export const Hero: React.FC<CardProps> = ({ img, alt, title, body }) => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={img} className="max-w-sm rounded-lg shadow-2xl" alt={alt} />
+        <div>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          <p className="py-6">{body}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const BlankCard: React.FC<CardProps> = ({ title, body }) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{body}</p>
+        <div className="card-actions justify-end">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ImageCard: React.FC<CardProps> = ({ img, alt, title, body }) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <figure><img src={img} alt={alt} /></figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+};
