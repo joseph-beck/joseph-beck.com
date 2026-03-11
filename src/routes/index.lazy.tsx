f769aa8020a4f5de003328771bc481a91246ef2e
Joseph Beck <joebeck03@icloud.com>
2026-03-11 21:09:19 +0000

    feat: update packages and shad

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,11 +1,14 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import type { ReactElement } from 'react'
 
+import { Button } from '@/ui/button'
+
 const Page = (): ReactElement => {
   return (
     <div className="p-2">
       <div className="flex justify-center">
         <div className="w-full sm:w-5/6 md:w-3/4 lg:w-2/3">test</div>
+        <Button>button</Button>
       </div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
     }),
+    react(),
     tailwindcss(),
   ],
   resolve: {
