9b38e2637c32cfa9c7eb31558dcb987537059852
Joseph Beck <joebeck03@icloud.com>
2026-03-11 21:00:10 +0000

    feat: start reset

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,25 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ReactElement } from 'react';
 
 import { ThemeProvider } from '@/components/theme/theme';
 import { ThemeToggle } from '@/components/theme/theme-toggle';
 
-export const Route = createRootRoute({
-  component: () => (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="p-2 flex gap-2">
-        <ThemeToggle />
-      </div>
-      <hr />
-      <div className="scroll-smooth">
-        <Outlet />
-      </div>
-      {process.env.NODE_ENV === 'development' && <TanStackRouterDevtools />}
-    </ThemeProvider>
-  ),
+const Root = (): ReactElement => (
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <div className="p-2 flex gap-2">
+      <ThemeToggle />
+    </div>
+    <hr />
+    <div className="scroll-smooth">
+      <Outlet />
+    </div>
+    <TanStackRouterDevtools />
+  </ThemeProvider>
+);
+
+const Route = createRootRoute({
+  component: Root,
 });
+
+export { Route };
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,30 +1,21 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import type { ReactElement } from 'react';
 
-import { ExperienceTimeline } from '@/components/experience/experience-timeline';
-import Footer from '@/components/footer/footer';
-import { Home } from '@/components/home/home';
-import { ProjectGrid } from '@/components/project/project-grid';
 import { BackgroundBeams } from '@/ui/background-beams';
-import { Separator } from '@/ui/separator';
 
-export const Route = createLazyFileRoute('/')({
-  component: Page,
-});
-
-function Page() {
+const Page = (): ReactElement => {
   return (
     <div className="p-2">
       <div className="flex justify-center">
-        <div className="w-full sm:w-5/6 md:w-3/4 lg:w-2/3">
-          <Home />
-          <Separator className="mb-6" />
-          <ProjectGrid />
-          <Separator className="mt-6" />
-          <ExperienceTimeline />
-        </div>
+        <div className="w-full sm:w-5/6 md:w-3/4 lg:w-2/3">test</div>
       </div>
-      <Footer />
       <BackgroundBeams className="-z-10 fixed opacity-75" />
     </div>
   );
-}
+};
+
+const Route = createLazyFileRoute('/')({
+  component: Page,
+});
+
+export { Route };
